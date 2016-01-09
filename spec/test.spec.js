5afc15fc91f5d22777ef101719e7662c7ba0e9ee
TakaakiFuruse <takaakifuruse@gmail.com>
2016-01-09 13:49:57 +0900

    done

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,14 +1,28 @@
-var _ = require('underscore')
+var _ = require('underscore');
 
+var htmlCodes = {
+    "&": "& amp;",
+    "<": "& lt;",
+    ">": "& gt;",
+    "\"": "& quot;",
+    "'": "& apos;"
+}
 
 function convert(str) {
-    // &colon;&rpar;
-    return str;
-}
+    var strAr = str.split('');
+    for (var i = str.length - 1; i >= 0; i--) {
+        for (var keys in htmlCodes) {
+            if (strAr[i] === keys) {
+                strAr[i] = htmlCodes[keys];
+            }
+        }
+    }
+    return strAr.join('')
+};
 
 describe('description', function() {
     it('1', function() {
-        expect(convert("Dolce & Gabbana")).toEqual("Dolce & ​amp; Gabbana");
+        expect(convert("Dolce & Gabbana")).toEqual('Dolce & ​amp; Gabbana');
         expect(convert("Hamburgers < Pizza < Tacos")).toEqual("Hamburgers & ​lt; Pizza & ​lt; Tacos");
         expect(convert("Sixty > twelve")).toEqual("Sixty & ​gt; twelve");
         expect(convert('Stuff in "quotation marks"')).toEqual("Stuff in & ​quot; quotation marks & ​quot;");
