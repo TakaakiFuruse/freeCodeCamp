84099549ba736dbb3431efae6ee0b40b47e99299
TakaakiFuruse <takaakifuruse@gmail.com>
2016-01-10 12:48:18 +0900

    done

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,42 +1,28 @@
 var _ = require('underscore');
 
 function sumFibs(num) {
-    var fibs = new Fibonaccier(num);
-    return fibs.fibOddsSum
-}
-
-function Fibonaccier(num) {
-    this.numAr = [];
-    this.fibNums = this.fibCalc(num);
-    this.fibOdds = this.fibOdds();
-    this.fibOddsSum = this.fibOddsSum();
-}
-
-Fibonaccier.prototype.fibCalc = function(num) {
-    this.numAr = [0, 1]
-    for (var i = 1; i < num; i++) {
-        f = this.numAr[this.numAr.length - 1] + this.numAr[this.numAr.length - 2]
-        this.numAr.push(f)
+    var fibAr = [0, 1, 1];
+    while (fibAr[fibAr.length - 1] < num + 1) {
+        fibAr.push(
+            fibAr[fibAr.length - 1] + fibAr[fibAr.length - 2]
+        );
     }
-}
+    fibAr.pop();
+    fibAr = fibAr.filter(function(index) {
+    return index % 2 !== 0;
+});
 
-Fibonaccier.prototype.fibOdds = function() {
-    return this.numAr.filter(function(elm) {
-        return elm % 2 !== 0;
-    });
-}
-
-Fibonaccier.prototype.fibOddsSum = function() {
-    return _.reduce(this.fibOdds, function(memo, value) {
+    return _.reduce(fibAr, function(memo, value) {
         return memo + value;
     }, 0);
 }
+
 describe('description', function() {
     it('description', function() {
-            expect(sumFibs(1000)).toEqual(1785)
-            expect(sumFibs(4000000)).toEqual(4613732)
-            expect(sumFibs(4)).toEqual(5)
-            expect(sumFibs(75024)).toEqual(60696)
-            expect(sumFibs(75025)).toEqual(135721)
+        expect(sumFibs(1000)).toEqual(1785)
+        expect(sumFibs(4000000)).toEqual(4613732)
+        expect(sumFibs(4)).toEqual(5)
+        expect(sumFibs(75024)).toEqual(60696)
+        expect(sumFibs(75025)).toEqual(135721)
     })
 })
